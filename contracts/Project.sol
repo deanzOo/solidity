2990d59039dbb410814033bf95603bbed18eb159
dean <deanabutbul@gmail.com>
2021-02-12 16:07:16 +0200

    got some first skeleton for the project

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -14,35 +14,87 @@ contract Project {
     uint start;
     uint expiration;
     address [] competitors;
-    mapping (address => uint)Project;
+    mapping (address => uint)Score;
+    mapping (address =>uint)Submissions;
 
     constructor()public payable {
         start = now;
         expiration = start + msg.duration * 1 days;
     }
 
-    modifier competitionExpired() {
+    // prevents accessing contract af
+    modifier competitionNotExpired() {
         require(
             now <= expiration
         );
         _;
     }
 
-    function endpoint()public {
-        // something from the sender
-        // with some random event occuring using the message
-        //
+    // prevents competitors to submit more the 3 times
+    modifier competitorSubmittedEnough(address _competitor) {
+        require(
+            !is_competitor(_competitor) || Submissions[_competitor] <= 3
+        );
+        _;
     }
 
-    function random_first()private pure {
-        competitors
+    // public endpoint for all competitors to submit their bets
+    function endpoint() public
+    competitionNotExpired competitorSubmittedEnough(msg.sender){
+        init_competitor(msg.sender);
+        uint func_num = chooseRandomFunction(msg.data);
+        if (func_num == 1)
+            random_first(msg.sender);
+        else if (func_num == 2)
+            random_second(msg.sender);
+        else random_third(msg.sender);
     }
 
-    function random_second()private pure {
-
+    /*******************************************************************************
+        functions that were chosen randomly'ish to decide scores for competitors
+    *********************************************************************************/
+    function random_first(address _competitor) private pure
+    {
+        Score[_competitor] += 50;
+        Submissions[_competitor] += 1;
     }
 
-    function random_third()private pure {
-
+    function random_second(address _competitor) private pure
+    {
+        Score[_competitor] += 100;
+        Submissions[_competitor] += 1;
     }
+
+    function random_third(address _competitor) private pure
+    {
+        Score[_competitor] += 200;
+        Submissions[_competitor] += 1;
+    }
+
+    /*******************************************************************************
+                                   Helper Functions
+    *********************************************************************************/
+    // simple way of checking if competitor already submitted or not
+    function is_competitor(address _sender) private pure returns (bool) {
+        for (i = 0; i < competitors.length; i++)
+            if (competitors[i] == _sender)
+                return true;
+        return false;
+    }
+
+    // if it's first submission for the competitor, lets enter him into the data base
+    function init_competitor(address _sender) private {
+        if (!is_competitor( _sender)) {
+            competitors.push( _sender);
+            Score[ _sender] = 0;
+            Submissions[ _sender] = 1;
+        }
+    }
+
+    // some random way of choosing a scoring for the competitor
+    // in real world situations, this will not be chosen randomly, but by specific answers and success
+    function chooseRandomFunction(uint _bet) private pure returns (uint){
+        return (uint(keccak256(block.difficulty, _bet) * keccak256(now)) % 3) + 1;
+    }
+
 }
