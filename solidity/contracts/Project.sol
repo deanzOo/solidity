467da9e97cac5283a5393fdc34f9bd69d54dc780
dean <deanabutbul@gmail.com>
2021-02-18 07:58:59 +0200

    almost finished. need to redo the getters for state vars. need to redo the getwinner function

--- solidity/contracts/Project.sol
+++ solidity/contracts/Project.sol
@@ -2,50 +2,63 @@
 pragma solidity ^0.7.4;
 
 contract Project {
-    // need 3 functions
-    // need some random way of activating any of these
-    // need to figure out how to get input from outside the contract
-    // need to use the input to affect the random usage of a function
-    // need to learn how to keep track of gas usage
-    // need to keep track of gas usage per address
-    // need to have function to close competition
-    // need to have function to return the points table and winner address
-
     uint start;
     uint expiration;
-    address payable [] competitors;
-    mapping (address => uint)Scores;
-    mapping (address => uint)Submissions;
-    uint [] formatted_scores;
+    address [] competitors;
+    uint256 [] Scores;
+    uint [] Submissions;
 
-    uint _LIM = 50;
+    uint _LIM = 17;
 
-    constructor(uint _daysActive) payable {
+    constructor(uint _daysActive) {
         start = block.timestamp;
         expiration = start + _daysActive * 1 days;
     }
 
-    // prevents competitors to submit more the 3 times
-    modifier competitorSubmittedEnough(address _competitor) {
-        require(
-            !is_competitor(_competitor) || Submissions[_competitor] <= 3 || this.expired()
-        );
-        _;
+    function getCompetitors() public view returns (address [] memory) {
+        int l = int(competitors.length);
+        address [l] memory comp_copy;
+        for (uint i = 0; i < competitors.length; i++)
+            comp_copy[i] = competitors[i];
+        return comp_copy;
+    }
+    function getScores() public view returns (uint256 [] memory) {
+        int l = int(Scores.length);
+        uint256 [l] memory scores_copy;
+        for (uint i = 0; i < Scores.length; i++)
+            scores_copy[i] = Scores[i];
+        return scores_copy;
+    }
+    function getSubmissions() public view returns (uint [] memory) {
+        int l = int(Submissions.length);
+        uint [l] memory submissions_copy;
+        for (uint i = 0; i < Submissions.length; i++)
+            submissions_copy[i] = Submissions[i];
+        return submissions_copy;
     }
 
     // public endpoint for all competitors to submit their bets
-    function endpoint(uint _answer) public
-    competitorSubmittedEnough(msg.sender){
-        if (this.expired()) {
-            uint max_index = 0;
-            for (uint i = 0; i < competitors.length; i++)
-                if (Scores[competitors[i]] > max_index)
-                    max_index = i;
-            selfdestruct(competitors[max_index]);
+    function bet(uint _answer) public {
+        if (!expired()) {
+            uint index = init_competitor(msg.sender);
+            if (Submissions[index] < 3) {
+                Submissions[index] += 1;
+                Scores[index] = random_gas_use(_answer);
+            }
         }
-        init_competitor(msg.sender);
-        Submissions[msg.sender] += 1;
-        Scores[msg.sender] = random_gas_use(_answer);
+    }
+
+    function getWinner() public returns (address){
+        if (expired()) {
+            uint256 max = 0;
+            uint maxIndex = 0;
+            for (uint i = 0; i < Scores.length; i++) {
+                if (Scores[i] > max)
+                    maxIndex = i;
+            }
+            return competitors[maxIndex];
+        }
+        return address(0);
     }
 
     function expired() public view returns (bool) {
@@ -57,38 +70,45 @@ contract Project {
     }
 
     // use gas randomly based on _limit_base
-    function random_gas_use(uint _limit_base) private view returns (uint)
+    function random_gas_use(uint _limit_base) public view returns (uint256)
     {
         uint limit = chooseRandomLimit(_limit_base);
-        uint old_gas_used = gasleft();
+        uint256 old_gas_used = gasleft();
+        uint fix = 0;
         for (uint i = 0; i < limit; i++) {
-            mulmod(block.difficulty, block.timestamp, limit);
+            fix += mulmod(block.difficulty, block.timestamp, limit);
         }
-        return gasleft() - old_gas_used;
+        return old_gas_used - gasleft();
     }
 
     /*******************************************************************************
                                    Helper Functions
     *********************************************************************************/
     // simple way of checking if competitor already submitted or not
-    function is_competitor(address _sender) private view returns (bool) {
+    function is_competitor(address _sender) public view returns (int) {
         for (uint i = 0; i < competitors.length; i++)
-            if (competitors[i] == _sender)
-                return true;
-        return false;
+            if (competitors[uint(i)] == _sender)
+                return int(i);
+        return -1;
     }
 
     // if it's first submission for the competitor, lets enter him into the data base
-    function init_competitor(address payable _sender) private {
-        if (!is_competitor(_sender)) {
+    function init_competitor(address _sender) public returns (uint){
+        int index = is_competitor(_sender);
+        if (index == -1) {
+            Scores.push(0);
+            Submissions.push(0);
             competitors.push(_sender);
+            return uint(competitors.length - 1);
         }
+
+        return uint(index);
     }
 
     // some random way of choosing a scoring for the competitor
     // in real world situations, this will not be chosen randomly, but by specific answers and success
     function chooseRandomLimit(uint _bet) private view returns (uint){
-        return addmod(_bet * block.timestamp, block.number * block.difficulty, _LIM);
+        return _bet % _LIM;
     }
 
 }
